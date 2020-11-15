48d9626f261dce3f8cd2fd6dd99df9a5764e842d
Liam Bradley <liamhbradley11@gmail.com>
2020-11-14 22:11:56 -0800

    deployment

---
+++ src/components/Head/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Head() {
+    return (
+<div className = "wrapper">
+    <div className = "title">
+Employee Directory
+    </div>
+    <br></br>
+    <div className = "tagline">
+Employee List And Detail Information
+    </div>
+</div>
+
+    );
+}
+
+export default Head;
